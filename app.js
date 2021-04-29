7222efa8fc12cf3dca60981753eab4af4e7ef6a2
connorHashDash <ckb1997@hotmail.co.uk>
2021-04-29 20:08:14 +0100

    fixed the bug and now all nodes get correctly deleted and refreshed

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ for (i = 0; i <= gridSize; ++i){
     let gridDiv = document.createElement('div')
     gridDiv.className = 'gridDivs'
     masterDiv.appendChild(gridDiv)
-    gridDiv.addEventListener('mouseover', function(e){
+    gridDiv.addEventListener('mouseover', function (e) {
         this.style.backgroundColor = 'black';
     });
 }
@@ -21,7 +21,7 @@ for (i = 0; i <= gridSize; ++i){
 
 function clearGrid(x){
     let gridSize = x * x;
-    for(j = 0; j <= gridSize; ++j){
+    for(j = 0; j <= gridSize - 1; ++j){
         if (masterDiv.childNodes.length >= 0){
         masterDiv.removeChild(masterDiv.childNodes[0])
         } else {
@@ -33,10 +33,10 @@ function clearGrid(x){
 
 changeRes.addEventListener('click', function(){
     clearGrid(gridInput);
-    gridInput = prompt('please select new resolution please put within 1 - 64 range')
+    gridInput = prompt('please select new resolution please put within 1 - 64 range');
     if (gridInput > 64 || gridInput < 1)  {
-        alert('Out of Range')
-        return
+        alert('Out of Range');
+        return;
     }
     makegrid(gridInput)
 });
