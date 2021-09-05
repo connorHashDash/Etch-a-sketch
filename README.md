3f2b2c851a1eecafa6233ac0d57109b067e16e4b
connorHashDash <ckb1997@hotmail.co.uk>
2021-09-05 20:18:06 +0100

    added readme

---
+++ README.md
@@ -0,0 +1,3 @@
+An etch a sketch that you can use by mousing over the squares in the grid.
+
+changing the resolution affects the amount of squares in the grid. 
--- app.js
+++ app.js
@@ -5,45 +5,37 @@ let gridInput = 16;
 
 
 //const gridAmount = prompt('Amount of rows and columns?')
-function makegrid(x){
-let gridSize = x * x - 1;
-    masterDiv.style.gridTemplateColumns =`repeat(${x}, 1fr)`
-    masterDiv.style.gridTemplateRows =`repeat(${x}, 1fr)`
-for (i = 0; i <= gridSize; ++i){
-    let gridDiv = document.createElement('div')
-    gridDiv.className = 'gridDivs'
-    masterDiv.appendChild(gridDiv)
-    gridDiv.addEventListener('mouseover', function (e) {
-        this.style.backgroundColor = 'black';
-    });
-}
+function makegrid(x) {
+    let gridSize = x * x - 1;
+    masterDiv.style.gridTemplateColumns = `repeat(${x}, 1fr)`
+    masterDiv.style.gridTemplateRows = `repeat(${x}, 1fr)`
+    for (i = 0; i <= gridSize; ++i) {
+        let gridDiv = document.createElement('div')
+        gridDiv.className = 'gridDivs'
+        masterDiv.appendChild(gridDiv)
+        gridDiv.addEventListener('mouseover', function(e) {
+            this.style.backgroundColor = 'black';
+        });
+    }
 }
 
-function clearGrid(x){
+function clearGrid(x) {
     masterDiv.innerHTML = ''
 }
 
-changeRes.addEventListener('click', function(){
+changeRes.addEventListener('click', function() {
     clearGrid(gridInput);
     gridInput = prompt('please select new resolution please put within 1 - 64 range');
-    if (gridInput > 64 || gridInput < 1)  {
+    if (gridInput > 64 || gridInput < 1) {
         alert('Out of Range');
         return;
     }
     makegrid(gridInput)
 });
 
-reset.addEventListener('click', function(){
+reset.addEventListener('click', function() {
     clearGrid(gridInput);
-    makegrid(gridInput)
+    makegrid(gridInput);
 });
 
 makegrid(gridInput)
-
-
-
-
-
-
-
-
